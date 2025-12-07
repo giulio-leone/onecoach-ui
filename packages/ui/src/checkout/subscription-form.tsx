8e077bdc10196a4f597699fbe7ead0cc7e7248f8
g97iulio1609 <giulio97.leone@gmail.com>
2025-12-07 17:48:42 +0100

    chore: bonify UI packages

--- packages/ui/src/checkout/subscription-form.tsx
+++ packages/ui/src/checkout/subscription-form.tsx
@@ -10,7 +10,7 @@ import { useState } from 'react';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import { Button } from '../button';
 import { CreditCard } from 'lucide-react';
-import { handleApiError } from '@OneCoach/lib-shared/utils/api-error-handler';
+import { handleApiError } from '@OneCoach/lib-shared';
 
 interface SubscriptionFormProps {
   clientSecret: string;
